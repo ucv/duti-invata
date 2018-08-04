31760a4005af59ff57ee2b3e7f4d0d3bbb42667e
Ungureanu C. Valentin <u.valentin89@gmail.com>
2018-08-04 19:10:42 +0300

    Duti invata sirul lui Fibonacci

---
+++ Invata Fibonacci/cmake-build-debug/CMakeFiles/Invata_Fibonacci.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Invata_Fibonacci.dir/main.cpp.o"
+  "Invata_Fibonacci.pdb"
+  "Invata_Fibonacci"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Invata_Fibonacci.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Invata Fibonacci/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int casuta_zero = 0;
+    int casuta_unn = 1;
+    int suma;
+    cout << casuta_zero << endl;
+    cout << casuta_unn << endl;
+    for (int i = 0; i < 20; ++i) {
+        suma = casuta_zero + casuta_unn;
+        casuta_zero = casuta_unn;
+        casuta_unn = suma;
+        cout << suma << endl;
+    }
+
+    return 0;
+}
